d8167711b8262cf08a08d8143c707321bad9d1ba
Prince-luck <herbertfortune12@gmail.com>
2024-07-31 13:23:23 +0100

    vc

--- config.js
+++ config.js
@@ -1,37 +1,59 @@
-//WOLF
+
+//#ENJOY
 const fs = require("fs-extra");
 if (fs.existsSync(".env"))
   require("dotenv").config({ path: __dirname + "/.env" });
-
-//=======[dependencies]====================//
-global.SESSION_ID = process.env.SESSION_ID || "";
-global.MONGODB = process.env.MONGODB_URI || "";
+global.audio = "";
+global.video = "";
+global.port = process.env.PORT;
+global.appUrl = process.env.APP_URL || "";
+global.email = "no@gmail.com";
+global.location = "Lagos, Nigeria";
+global.mongodb = process.env.MONGODB_URI || "";
+global.allowJids = process.env.ALLOW_JID || "null";
+global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
-global.sudo = process.env.SUDO
-  ? process.env.SUDO.replace(/[\s+]/g, "")
-  : "null";
-global.owner = process.env.OWNER_NUMBER
-  ? process.env.OWNER_NUMBER.replace(/[\s+]/g, "")
-  : "2349129361416";
-global.THUMB_IMAGE =
-  process.env.THUMB_IMAGE ||
-  process.env.IMAGE ||
-  "https://i.imgur.com/Xiqru2L.jpeg,https://i.imgur.com/Izl8wT1.jpeg";
-global.userImages =
-  process.env.USER_IMAGES ||
-  "https://i.imgur.com/Xiqru2L.jpeg,https://i.imgur.com/Izl8wT1.jpeg,https://i.imgur.com/Xiqru2L.jpeg,https://i.imgur.com/Izl8wT1.jpeg,https://i.imgur.com/Izl8wT1.jpeg";
-///===========[global iMPORTS]====================//
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Nigeria/Lagos";
+global.github = process.env.GITHUB || "https://github.com/Prince-luck/ZZZ-ban";
+global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029Vacs3IIIiRp0FeDDcJ1G";
+global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vacs3IIIiRp0FeDDcJ1G";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://i.imgur.com/Xiqru2L.jpeg,https://i.imgur.com/Izl8wT1.jpeg";
+global.devs = "none";
+global.sudo = process.env.SUDO || "";
+global.owner = process.env.OWNER_NUMBER || "";
+global.style = process.env.STYLE || "3";
+global.gdbye = process.env.GOODBYE || "false";
+global.wlcm = process.env.WELCOME || "false";
+global.warncount = process.env.WARN_COUNT || 3;
+global.disablepm = process.env.DISABLE_PM || "false";
+global.disablegroup = process.env.DISABLE_GROUPS || "false",
+global.MsgsInLog = process.env.MSGS_IN_LOG || "false";
+global.userImages = process.env.USER_IMAGES || "https://i.imgur.com/Xiqru2L.jpeg,https://i.imgur.com/Izl8wT1.jpeg";
+global.waPresence = process.env.WAPRESENCE || "available";
+global.readcmds = process.env.READ_COMMAND || "false";
+global.readmessage = process.env.READ_MESSAGE || "false";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
+global.read_status = process.env.AUTO_READ_STATUS || "true";
+global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status_from = process.env.SAVE_STATUS_FROM || "";
+global.read_status_from = process.env.READ_STATUS_FROM || "";
 
+global.api_smd = "https://api-smd-1.vercel.app";
+global.scan = "https://secret-garden-43998-4daad95d4561.herokuapp.com/";
+
+global.SESSION_ID =
+  process.env.SESSION_ID ||
+  ""
 module.exports = {
-  menu: process.env.MENU || "",
+  menu: process.env.MENU || "2",
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "`ZZZ-V1`",
-  author: process.env.PACK_AUTHER || "ZZZ-V1",
+  caption: process.env.CAPTION || "`WOLF-V2™`",
+  author: process.env.PACK_AUTHER || "WOLF-V2",
   packname: process.env.PACK_NAME || "WOLF",
-  botname: process.env.BOT_NAME || "ZZZ-V1",
-  ownername: process.env.OWNER_NAME || " WOLF ",
+  botname: process.env.BOT_NAME || "WOLF-V2",
+  ownername: process.env.OWNER_NAME || " WOLF",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -42,38 +64,10 @@ module.exports = {
   HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
   aitts_Voice_Id: process.env.AITTS_ID || "37",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
-  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
-  LANG: (process.env.THEME || "WhatsApp").toUpperCase(),
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "private",
+  LANG: (process.env.THEME || "WOLF").toUpperCase(),
 };
-global.port = process.env.PORT;
-global.appUrl = process.env.APP_URL || "https://whatsapp.com/channel/0029Vacs3IIIiRp0FeDDcJ1G";
-global.email = "unkwon@gmail.com";
-global.location = "";
-global.allowJids = process.env.ALLOW_JID || "null";
-global.blockJids = process.env.BLOCK_JID || "null";
-global.timezone = process.env.TZ || process.env.TIME_ZONE || "Africa/Lagos";
-global.github = process.env.GITHUB || "https://github.com/Prince-luck/Test";
-global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029Vacs3IIIiRp0FeDDcJ1G";
-global.website = process.env.GURL || "https://whatsapp.com/channel/0029Vacs3IIIiRp0FeDDcJ1G";
-global.devs = "2349066528353";
-global.msg_style = process.env.STYLE || "4";
-global.session_reset = process.env.SS_RESET || "false";
-global.gdbye = process.env.GOODBYE || "false";
-global.wlcm = process.env.WELCOME || "false";
-global.warncount = process.env.WARN_COUNT || 3;
-global.disablepm = process.env.DISABLE_PM || "false";
-(global.disablegroup = process.env.DISABLE_GROUPS || "false"),
-  (global.MsgsInLog = process.env.MSGS_IN_LOG || "true");
-global.waPresence = process.env.WAPRESENCE || "online";
-global.readcmds = process.env.READ_COMMAND || "false";
-global.readmessage = process.env.READ_MESSAGE || "false";
-global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null";
-global.read_status = process.env.AUTO_READ_STATUS || "false";
-global.save_status = process.env.AUTO_SAVE_STATUS || "true";
-global.save_status_from = process.env.SAVE_STATUS_FROM || "null";
-global.read_status_from = process.env.READ_STATUS_FROM || "null";
-global.api_smd = "https://api.maher-zubair.tech";
-global.scan = "https://mainv2-f66485a0f702.herokuapp.com/";
+global.rank = "updated";
 global.isMongodb = false;
 let file = require.resolve(__filename);
 fs.watchFile(file, () => {
